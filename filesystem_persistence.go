88d7d248199a9fe21e2655009b6308e06ca6b0ec
Adam Eijdenberg <adam@continusec.com>
2017-04-06 09:41:58 +1000

    Remove left over debug statements

--- filesystem_persistence.go
+++ filesystem_persistence.go
@@ -40,9 +40,7 @@ type FilesystemPersistence struct {
 
 // Load returns value if found, nil otherwise
 func (f *FilesystemPersistence) Load(ctx context.Context, key []byte) ([]byte, error) {
-	path := filepath.Join(f.Dir, hex.EncodeToString(key))
-	log.Printf("Loading from disk: %s...", path)
-	rv, err := ioutil.ReadFile(path)
+	rv, err := ioutil.ReadFile(filepath.Join(f.Dir, hex.EncodeToString(key)))
 	if err != nil {
 		if os.IsNotExist(err) {
 			return nil, ErrStorageKeyNotFound
@@ -59,9 +57,6 @@ func (f *FilesystemPersistence) Save(ctx context.Context, key, value []byte, ttl
 		log.Println("Persistence layer is configured as immutable, yet tool is trying to write to it")
 		return ErrInvalidConfig
 	}
-
-	path := filepath.Join(f.Dir, hex.EncodeToString(key))
-	log.Printf("Saving to disk: %s...", path)
 
 	// Create dir if not exists
 	_, err := os.Stat(f.Dir)
@@ -98,7 +93,7 @@ func (f *FilesystemPersistence) Save(ctx context.Context, key, value []byte, ttl
 	}
 
 	// Rename to correct filename - while not guaranteed by Golang to be atomic, apparently POSIX does on *nix systems
-	err = os.Rename(tf.Name(), path)
+	err = os.Rename(tf.Name(), filepath.Join(f.Dir, hex.EncodeToString(key)))
 	if err != nil {
 		os.Remove(tf.Name()) // ignore failure
 		return err
